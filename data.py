c2f5c9a8c06e2833ea1e4be15d6fc13646414e9e
pilou-K75VJ <37050307+pilou-K75VJ@users.noreply.github.com>
2021-02-04 19:14:23 +0100

    data

--- data.py
+++ data.py
@@ -1,17 +1,18 @@
-import pandas as pd
 import os
+
+import pandas as pd
 
 
 def txt2csv(txtpath):
     path, ext = os.path.splitext(txtpath)
     if ext != '.txt':
-        return None
+        raise ValueError('Requires a .txt file but got {}'.format(txtpath))
     csvpath = path + '.csv'
     with open(txtpath, 'r') as txtfile, open(csvpath, 'w') as csvfile:
         for line in txtfile:
             if line[:5] == '$$SOE':
                 break
-        csvfile.write("timestamp,daylight,moon,EcLon,EcLat,\n")
+        csvfile.write("timestamp,daylight,moon,ecliptic_longitude,ecliptic_latitude,\n")
         for line in txtfile:
             if line[:5] == '$$EOE':
                 break
@@ -24,14 +25,13 @@ def parse_horizons(body=None):
         body = str()
 
     data = dict()
-    for filename in os.listdir('horizons/'):
+    files = os.listdir('horizons/')
+    for filename in files:
         name, ext = os.path.splitext(filename)
-        if body not in name:
+        if body not in name or ext != '.txt':
             continue
 
         filepath = txt2csv(os.path.join('horizons/', filename))
-        if filepath is None:
-            continue
 
         df = pd.read_csv(filepath)[['timestamp', 'EcLon']]
         data[name] = pd.Series(
@@ -40,8 +40,3 @@ def parse_horizons(body=None):
         )
 
     return pd.DataFrame(data)
-
-
-horizons = parse_horizons()
-
-print('ok')
